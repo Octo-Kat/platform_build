3c15550576b95a32d0842372af9386e7d9132767
broodplank <guichardflorent@gmail.com>
2014-04-19 04:27:25 +0200

    Add inclusion of optimized dalvik libs
    Conflicts:
    	core/config.mk

--- core/config.mk
+++ core/config.mk
@@ -493,6 +493,9 @@ RS_PREBUILT_CLCORE := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libclcore.bc
 RS_PREBUILT_LIBPATH := -L prebuilts/ndk/8/platforms/android-9/arch-$(TARGET_ARCH)/usr/lib
 RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcompiler_rt.a
 
+# Include Optimized dalvik
+include build/vendor/lib/lib.mk
+
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
