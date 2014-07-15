779963a67554316088cb7f538680a2f0e6bdcdd4
treken <chupekevin@gmail.com>
2014-07-15 10:05:24 -0400

    Fix build: cannot find oct.mk

--- core/product_config.mk
+++ core/product_config.mk
@@ -184,13 +184,9 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(OCT_BUILD),)
-    all_product_configs := $(shell ls device/*/$(OCT_BUILD)/oct.mk)
-  else
-    # Read in all of the product definitions specified by the AndroidProducts.mk
-    # files in the tree.
-    all_product_configs := $(get-all-product-makefiles)
-  endif # OCT_BUILD
+# Read in all of the product definitions specified by the AndroidProducts.mk
+# files in the tree.
+all_product_configs := $(get-all-product-makefiles)
 endif
 
 ifeq ($(OCT_BUILD),)
